9a471b8cb432677481e1f0ac08189c4c1ce3e933
NurbekLDM <jumaboyevabarno80@gmail.com>
2025-03-20 16:52:10 +0500

    fixed some issue

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,9 @@ import userModel from "../models/userModel.js";
 
 const auth = async (req, res, next) => {
   try {
-    const token = req.cookies.token;
+    const token = req.cookies.token || 
+        req.headers.authorization?.split(' ')[1] || 
+        req.header('x-auth-token');
 
     if (!token) {
       return res.status(401).json({ message: "Authentication required" });
